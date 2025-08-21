7cd4eae706e8d4d2293f21468bae6e42159baea0
Gumnov Ruslan <ruslang.crocode@gmail.com>
2025-08-21 14:24:51 +0300

    add bg to 404 page

--- src/components/pages/404/index.tsx
+++ src/components/pages/404/index.tsx
@@ -1,17 +1,17 @@
 import styles from './styles.module.scss';
 import { Button } from '@/components/ui';
+import Image from 'next/image';
 
 const NotFoundPage = () => {
   return (
-    <section className={styles.page}>
+   <section className={styles.page}>
       <div className={styles.page__inner}>
         <h1 className={styles.page__title}>404</h1>
         <span className={styles.page__text}>Page Not Found</span>
         <p className={styles.page__text}>The page you're looking for doesn't exist or has been moved</p>
-        <Button as='link' href='/' className={styles.button}>
-          <span className={styles.button__text}>Return to home</span>
-        </Button>
+        <Button as='link' href='/' className={styles.button} text='Return to home'/>
       </div>
+      <Image className={styles.page__bg} src={'/images/bg/bg.svg'} width={1920} height={1433} alt={"backgroud image"}/>
     </section>
   )
 }
