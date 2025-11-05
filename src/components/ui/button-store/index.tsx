34baef668b82b7573d286fa12e2e71960eb954d9
Gumnov Ruslan <ruslang.crocode@gmail.com>
2025-11-05 10:45:02 +0300

    fix policy

--- src/components/ui/button-store/index.tsx
+++ src/components/ui/button-store/index.tsx
@@ -13,6 +13,7 @@ type TButton = {
   icon: React.ReactNode;
   text: string;
   title: string;
+  href?: string;
 };
 
 const BUTTONS_DATA: Record<StoreType, TButton> = {
@@ -25,6 +26,7 @@ const BUTTONS_DATA: Record<StoreType, TButton> = {
     icon: <GooglePlayIcon />,
     text: 'Get on',
     title: 'Google Play',
+    // href: 'https://play.google.com/store/search?q=Kaelis&c=apps&hl=ru',
   },
 };
 
@@ -37,17 +39,39 @@ const ButtonStore = ({ type, className }: TProps) => {
   const {openModal} = useModalContext()
   const button = BUTTONS_DATA[type];
 
-  return (
-    <Button as='button' className={`${styles.button} ${className}`} onClick={openModal}>
-      <div className={styles.button__inner}>
-        <span className={styles.button__icon}>{button.icon}</span>
-        <div className={styles.button__content}>
-          <span className={styles.button__text}>{button.text}</span>
-          <span className={styles.button__title}>{button.title}</span>
-        </div>
-      </div>
+  const handleClick = () => {
+    if (!!button.href) {
+      if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+        window.gtag('event', type === 'app' ? 'click_appstore' : 'click_googleplay', {
+          page_path: window.location.pathname,
+        });
+      }
+    } else {
+      openModal();
+    }
+  };
+
+  return button?.href ? (
+    <Button as={'link'} href={button.href} className={`${styles.button} ${className}`} onClick={handleClick}>
+      <InnerButton {...button}/>
     </Button>
-  );
+  ) : (
+    <Button as={'button'} className={`${styles.button} ${className}`} onClick={handleClick}>
+      <InnerButton {...button}/>
+    </Button>
+  )
 };
 
 export default ButtonStore;
+
+function InnerButton({icon, text, title}: TButton) {
+    return (
+      <div className={styles.button__inner}>
+        <span className={styles.button__icon}>{icon}</span>
+        <div className={styles.button__content}>
+          <span className={styles.button__text}>{text}</span>
+          <span className={styles.button__title}>{title}</span>
+        </div>
+      </div>
+    )
+  }
