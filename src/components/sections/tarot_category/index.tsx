c161c19b4a7c59f6596d631ffe37c12477adc314
Gumnov Ruslan <ruslang.crocode@gmail.com>
2026-06-01 16:36:57 +0300

    test2

--- src/components/sections/tarot_category/index.tsx
+++ src/components/sections/tarot_category/index.tsx
@@ -1,47 +1,49 @@
-import styles from './styles.module.scss'
+'use client';
+
+import styles from './styles.module.scss';
 
 import { CategoriesGrid, CategoryCard } from '@/components/categories';
 import { useTranslations, useLocale } from 'next-intl';
 import { TarotCategory } from '@/lib/types/shuffle';
-import { shuffleActions, useAppDispatch } from '@/store';
+import { shuffleActions, useAppDispatch, useAppSelector } from '@/store';
 import { Breadcrumbs } from '@/components/ui';
 import { TBreadcrumbs } from '@/types/breadcrumbs';
 
 type TProps = {
   categories: TarotCategory[] | null;
-}
+};
 
-const TarotCategorySection = ({categories}: TProps) => {
-  const t = useTranslations()
-  const locale = useLocale()
-  const dispatch = useAppDispatch()
+const TarotCategorySection = () => {
+  const t = useTranslations();
+  const locale = useLocale();
+  const dispatch = useAppDispatch();
+  const { categories, isLoading } = useAppSelector((state) => state.shuffle);
 
   const breadcrumbsData: TBreadcrumbs[] = [
     {
       label: t('breadcrumbs.home'),
-      url: '/'
+      url: '/',
     },
     {
       label: t('breadcrumbs.tarot'),
-      url: '/tarot'
-    }
-  ]
+      url: '/tarot',
+    },
+  ];
 
   const onClickSelectCategory = (category: TarotCategory) => {
-    dispatch(shuffleActions.setSelectedCategory({data: category, lang: locale}))
-  }
+    dispatch(shuffleActions.setSelectedCategory({ data: category, lang: locale }));
+  };
 
   return (
     <section className={styles.tarot}>
-
       <div className={styles.tarot__header}>
         <Breadcrumbs data={breadcrumbsData} />
         <h1 className={styles.tarot__title}>{t('CategoriesPage.title')}</h1>
         <p className={styles.tarot__description}>{t('CategoriesPage.subtitle')}</p>
       </div>
-      
+
       <CategoriesGrid>
-        {categories?.map((category: TarotCategory) => (
+        {categories.data?.map((category: TarotCategory) => (
           <CategoryCard
             key={category.id}
             id={String(category.id)}
@@ -54,8 +56,7 @@ const TarotCategorySection = ({categories}: TProps) => {
         ))}
       </CategoriesGrid>
     </section>
-    
-  )
-}
+  );
+};
 
-export default TarotCategorySection
+export default TarotCategorySection;
