da44b750d11a37500a4ea82efc57420415720f01
Gumnov Ruslan <ruslang.crocode@gmail.com>
2026-06-05 13:30:29 +0300

    fixed interpretation opening

--- src/components/ui/question/index.tsx
+++ src/components/ui/question/index.tsx
@@ -1,40 +1,42 @@
-'use client'
+'use client';
 
 import styles from './styles.module.scss';
 
 import { useRef } from 'react';
 import { TFAQ_DATA } from '@/components/sections/faq/data';
+import cn from 'classnames';
 
 type TProps = {
   data: TFAQ_DATA;
   isOpen: boolean;
-  setIsOpen: () => void
-}
+  setIsOpen: () => void;
+  className?: string;
+  classNameTitle?: string;
+};
 
-const Question = ({data, isOpen, setIsOpen}: TProps) => {
-  const itemRef = useRef<HTMLDivElement>(null)
+const Question = ({ data, isOpen, setIsOpen, className, classNameTitle }: TProps) => {
+  const itemRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className={styles.question} onClick={setIsOpen}>
+    <div className={cn(styles.question, className)} onClick={setIsOpen}>
       <button type='button' className={styles.question__header}>
-        <span className={styles.question__question}>{data.question}</span>
-        <span className={`${styles.question__plus} ${isOpen ? styles['question__plus--open'] : ''}`}/>
+        <span className={cn(styles.question__question, classNameTitle)}>{data.question}</span>
+        <span className={cn(styles.question__plus, isOpen ? styles['question__plus--open'] : '')} />
       </button>
-      <div 
-      className={`${styles.question__content}`}
-      style={
-        isOpen && itemRef.current 
-          ? { height: itemRef.current.scrollHeight } 
-          : { height: '0px' }
-      }>
-        <p 
-          className={styles.question__content_inner} 
-          ref={itemRef} 
-          dangerouslySetInnerHTML={{__html: data.answer}}
+      <div
+        className={`${styles.question__content}`}
+        style={
+          isOpen && itemRef.current ? { height: itemRef.current.scrollHeight } : { height: '0px' }
+        }
+      >
+        <p
+          className={styles.question__content_inner}
+          ref={itemRef}
+          dangerouslySetInnerHTML={{ __html: data.answer }}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
